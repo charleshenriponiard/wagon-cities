30904469e7dc5f42758a82fe5c18831d3e709762
charles-henri poniard <c.poniard@gmail.com>
2020-04-02 16:33:33 +0200

    opti

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import citiesReducer from './reducers/cities_reducer';
 import selectedCityReducer from './reducers/selected_city_reducer';
 
@@ -17,8 +17,10 @@ const reducers = combineReducers({
 });
 
 // render an instance of the component in the DOM
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={createStore(reducers, composeEnhancers() )}>
     <App />
   </Provider>,
   document.getElementById('root')
